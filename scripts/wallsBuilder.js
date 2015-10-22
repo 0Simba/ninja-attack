4c09043aa6e594da80d6839aeed2e757f030845a
0Simba <b.dersoir@student.isartdigital.com>
2015-10-22 15:59:27 +0200

    add wall texture

--- scripts/wallsBuilder.js
+++ scripts/wallsBuilder.js
@@ -4,12 +4,18 @@ define([
     'use strict';
 
     return function (scene, walls) {
+        var material = new BABYLON.StandardMaterial('wall material', scene);
+        var texture  = new BABYLON.Texture('./assets/wall.jpg', scene);
+
+        material.diffuseTexture = texture;
+
         for (var i = 0; i < walls.length; i++) {
             var wallConfig = walls[i];
 
             var wall = BABYLON.Mesh.CreateBox("wall", {height: wallConfig.height, width : wallConfig.width, length : 2}, scene);
             wall.position        = new BABYLON.Vector3(wallConfig.x, wallConfig.y, 0);
             wall.checkCollisions = true;
+            wall.material        = material;
         };
     }
 });
