f37ae31558b7aabb70fd6050259ec764f1724a9e
0Simba <b.dersoir@student.isartdigital.com>
2015-10-22 13:40:19 +0200

    collectibles is ok

--- scripts/collectiblesBuilder.js
+++ scripts/collectiblesBuilder.js
@@ -1,11 +1,14 @@
 define([
-    'babylon'
-], function (BABYLON) {
+    'babylon',
+    './player'
+], function (BABYLON, player) {
     'use strict';
 
 
     function CollectiblesBuilder () {
-        this.list = [];
+        this.list     = [];
+        this.picked   = [false, false, false];
+        this.nbPicked = 0;
     }
 
     CollectiblesBuilder.prototype.build = function (scene, collectiblesData) {
@@ -18,15 +21,36 @@ define([
     };
 
 
+    CollectiblesBuilder.prototype.pick = function (collectible) {
+        this.nbPicked++;
+
+        var index = this.list.indexOf(collectible);
+        this.picked[index] = true;
+
+        collectible.mesh.dispose();
+    };
+
+    var collectiblesBuilder = new CollectiblesBuilder();
+
 
     function Collectible (scene, data) {
         var material = new BABYLON.StandardMaterial("material", scene);
         material.emissiveColor = new BABYLON.Color3(1, 1, 0);
 
+        var that = this;
+
+
         this.mesh = BABYLON.Mesh.CreateCylinder("collectible", 0.3, 0.3, 0.3, 20, scene);
         this.mesh.position = new BABYLON.Vector3(data.x, data.y, 0);
         this.mesh.material = material;
+
+        this.mesh.actionManager = new BABYLON.ActionManager(scene);
+        this.mesh.actionManager.registerAction(
+            new BABYLON.ExecuteCodeAction({ 'trigger' : BABYLON.ActionManager.OnIntersectionEnterTrigger, parameter : player.mesh}, function () {
+                collectiblesBuilder.pick(that);
+            }
+        ));
     }
 
-    return new CollectiblesBuilder();
+    return collectiblesBuilder;
 });
